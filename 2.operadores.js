42a293b463e17e23022ad3bf53f16dd743e0cc74
Antonio <toni12tineo@gmail.com>
2026-07-23 20:12:03 +0200

    clase 2

---
+++ 2.operadores.js
@@ -0,0 +1,47 @@
+// TIPOS DE DATOS
+const string = 'Hola soy un string'
+const numeros = 20.3
+const booleans = true //false
+
+const arrays = ["Primero", "Segundo",2,true]
+const nulo = null;
+const noDefinidio = undefined;
+
+
+// OPERADORES ARITMETICOS
+let x = 10
+x += 5
+console.log(x)
+x -= 1
+console.log(x)
+x *= 2
+console.log(x)
+x /= 2
+console.log(x)
+x %= 2
+console.log(x)
+
+const z = "1" + "1"
+console.log(z)
+
+
+// OPERADORES LOGICOS
+// & -> AND -> Y
+
+console.log(true && true)
+// || -> OR -> O
+console.log(true || false)
+// ! -> NOT -> NO
+console.log(!true)
+// IGUAL QUE
+console.log(10 == 10)
+//  ESTTRICTAMENTE IGUAL QUE
+console.log(10 === 10)
+// DESIGUAL QUE 
+console.log(10 !== 10)
+
+// MAYOR - MENOR
+console.log(5 > 5)
+console.log(5 >= 5)
+console.log(5 < 5)
+console.log(5 <= 5)
