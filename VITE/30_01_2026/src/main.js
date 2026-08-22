b6311301d0a79bc5caa7a0354dde9f587aaeb0c9
Antonio <toni12tineo@gmail.com>
2026-08-22 17:29:22 +0200

    actuuuuu

--- VITE/30_01_2026/src/main.js
+++ VITE/30_01_2026/src/main.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import Header from "./components/Header/Header.js";
+import Main from "./components/Main/Main.js";
 import Footer from "./components/Footer/Footer.js";
 
 //Indico al documento, que cuando todo el contenido del DOM esté cargado, me inyecte este HTML
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#app").innerHTML = `
   ${Header()}
+  ${Main()}
   ${Footer()}
 `;
 });
