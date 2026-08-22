efe7877c500c5007672d3f375d061cf427d5fd7d
Antonio <toni12tineo@gmail.com>
2026-08-22 21:51:33 +0200

    finish

---
+++ 13_02_20206/ejemplo_asincronia.js
@@ -0,0 +1,15 @@
+const promesa = new Promise ((resolve, reject)=> {
+const todoBien = true
+
+    if(todoBien){
+        resolve("Hemos conseguido resolver la promesa")
+    }else{
+        reject("Promesa rechazada")
+    }
+})
+promesa.then((valor) => {
+    console.log("OK", valor)
+})
+.catch((error)=>{
+    console.log("NO", error)
+})
