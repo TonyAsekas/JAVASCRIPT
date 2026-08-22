8ecb09eaa3dafa664a49ad27017c08d1fd6601e8
Antonio <toni12tineo@gmail.com>
2026-08-22 18:44:15 +0200

    react

---
+++ 06_02_2026/src/App.jsx
@@ -0,0 +1,11 @@
+import Avatar from './components/avatar';
+import './App.css'
+
+const App=() => {
+  return<>
+  <Avatar username ="Peter Parker" imageURL="https://i.pinimg.com/736x/05/82/0f/05820ffbb40e4679a91891016b858a8b.jpg"/>
+  <Avatar username = "Ciclope" size ="sm"/>
+  </>;
+}
+
+export default App;
