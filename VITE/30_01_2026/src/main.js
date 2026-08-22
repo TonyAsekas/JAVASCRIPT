13247ac3791ab2a43154f504f4e8ea4bc2a685fe
Antonio <toni12tineo@gmail.com>
2026-08-22 16:46:23 +0200

    actu

--- VITE/30_01_2026/src/main.js
+++ VITE/30_01_2026/src/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+import Header from './components/Header/Header'
+
+//Indico al documento,
+//que cuando todo el contenido del DOM esté cargado,
+//me inyecte este HTML
+
+document.addEventListener("DOMContentLoaded", () => {
+
+document.querySelector("#app").innerHTML= `
+ ${Header()}
+`
+})
