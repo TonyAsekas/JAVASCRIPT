933d5baee534f1e5d47b31a3f08e5eb6658a01f4
Antonio <toni12tineo@gmail.com>
2026-08-23 12:43:57 +0200

    actu

--- 27_02_2026/app.js
+++ 27_02_2026/app.js
@@ -1,5 +1,40 @@
+let lista = []
+
 document.querySelector("#themeBtn").addEventListener("click",()=>{
-    document.body.classList.toggle("dark")
+    if(document.body.className === "dark"){
+        document.body.className = "ligth"
+        localStorage.setItem("theme","ligth")
+    }else{
+        document.body.className = "dark"
+        localStorage.setItem("theme","dark")
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    if(localStorage.getItem("theme")){
+        document.body.className = localStorage.getItem("theme")
+    }
+
+    if(localStorage.getItem("lista")){
+        lista =JSON.parse(localStorage.getItem("lista"))
+        renderList(JSON.parse(localStorage.getItem("lista")))
+    }
 })
 
 
+
+document.querySelector("#listaBtn").addEventListener("click", 
+    () => {
+    lista.push(Math.floor(Math.random() * 100))
+    renderList(lista)
+    localStorage.setItem("lista",JSON.stringify(lista))
+})
+
+const renderList = (lista) => {
+     document.querySelector("#listadiv").innerHTML = ""
+    for (const elemento of lista) {
+        document.querySelector("#listadiv").innerHTML += `
+        <p>${elemento}</p>
+        `
+    }
+}
