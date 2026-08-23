9ce5c6540679db09e5cc92d379b2d2457bce2ded
Antonio Alvarez Fernández <toni12tineo@gmail.com>
2026-08-23 11:22:55 +0200

    APIS

---
+++ API's/20_02_2026/app.js
@@ -0,0 +1,65 @@
+// Funcion que busca el tiempo en la API con la ciduad que queramos y limpia los datos
+
+const getWeather = async (city) => {
+    const res = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=69902c6c30a342d6b7e163258241906&q=${city}&aqi=no`)
+    const data = await res.json()
+
+    //Limpiamos los datos
+    
+    const weatherData = {
+        ciudad : data.location.name,
+        pais: data.location.country,
+        temp: data.current.temp_c,
+        icono: data.current.condition.icon,
+        humedad: data.current.humidity,
+        sensacion: data.current.windchill_c
+    }
+    renderWeather(weatherData)
+}
+
+//Funcion que pinta los datos del tiempo en la interfaz
+
+const renderWeather = (data) => {
+   document.querySelector("main").innerHTML =`
+   <h2>${data.ciudad} - ${data.pais}</h2>
+    <h3>${data.temp}</h3>
+    <img src ="${data.icono}" alt = "Representa visual del tiempo"/>
+    <div>
+        <p>🌡️${data.sensacion}º</p>
+        <p>💧${data.humedad}%</p>
+    </div>
+   `
+}
+
+//Añadimos un evento click al boto para poder buscar otras ciudades
+
+document.querySelector("#searchBtn").addEventListener("click", () => {
+    const valorCiudad = document.querySelector("#city").value
+    getWeather(valorCiudad)
+    document.querySelector("#city").value =""
+})
+
+//Añadimos un evento a la tecla Enter
+
+document.querySelector("#city").addEventListener("keydown", (ev) => {
+    if(ev.key === "Enter"){
+        const valorCiudad = document.querySelector("#city").value
+        getWeather(valorCiudad)
+        document.querySelector("#city").value =""
+    }
+})
+
+// Al arranque de la aplicacion al renderizar el DOM por completo,
+// se busca el tiempo predeterminado
+
+    document.addEventListener("DOMContentLoaded", () => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+           const coord = `${pos.coords.latitude} , ${pos.coords.longitude}`
+             if(coord){
+                getWeather(coord)
+             }else{
+             getWeather("Madrid")
+             }
+            })
+        })
